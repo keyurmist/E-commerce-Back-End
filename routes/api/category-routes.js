adea29a84bc36f83b35d158471d5f51d4b0d6925
KeyurMistry <keyurmist@gmail.com>
2022-09-08 10:39:17 +0100

    added post to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -42,8 +42,16 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.json(newCategory);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 router.put("/:id", (req, res) => {
