5f3637a275ca47bce074a9598349d4508c9ec056
KeyurMistry <keyurmist@gmail.com>
2022-09-08 10:17:02 +0100

    finished relationships in models

--- models/index.js
+++ models/index.js
@@ -10,10 +10,22 @@ Product.belongsTo(Category, {
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "SET NULL",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
